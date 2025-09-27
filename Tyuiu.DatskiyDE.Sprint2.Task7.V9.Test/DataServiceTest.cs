ee0331d065b514f91a4d7743eeaea20966c324d0
BillyHarrington1 <datskiydenis@gmail.com>
2025-09-27 23:54:30 +0500

    Task7

---
+++ Tyuiu.DatskiyDE.Sprint2.Task7.V9.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.DatskiyDE.Sprint2.Task7.V9.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint2.Task7.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            // Точка внутри заштрихованной области
+            Assert.IsTrue(ds.CheckDotInShadedArea(0, 0.3));   
+
+            // Точка на границах
+            Assert.IsTrue(ds.CheckDotInShadedArea(0, 0.5));   
+
+            // Точка вне заштрихованной области
+            Assert.IsFalse(ds.CheckDotInShadedArea(0, 0.6));  
+          
+        }
+    }
+}
