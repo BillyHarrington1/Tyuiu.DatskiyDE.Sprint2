377e3419413d9c0862e77425fdf689207c3d63e3
BillyHarrington1 <datskiydenis@gmail.com>
2025-09-27 19:13:32 +0500

    Task4

---
+++ Tyuiu.DatskiyDE.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DatskiyDE.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 277.175;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DatskiyDE.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.DatskiyDE.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint2.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #2 | Выполнил: Дацкий Д. Е. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Операции сравнения                                               *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #11                                                            *");
+            Console.WriteLine("* Выполнил: Дацкий Денис Евгеньевич. | ИСПб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* написать программу, которая вычисляет требуемое значение c             *");
+            Console.WriteLine("* использованием тернарного оператора, ввод x,y, если                    *");
+            Console.WriteLine("* z = x - 20 * 2 < y / 4 ? (3 + 8 / (x, 2),y) : y - (x + 1 / y + 2, x);  *");
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+
+            Console.WriteLine("Введите x");
+            double x = Convert.ToInt32(Console.ReadLine());
+  
+
+            Console.WriteLine("Введите y");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            double res = ds.Calculate(x,y);
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
