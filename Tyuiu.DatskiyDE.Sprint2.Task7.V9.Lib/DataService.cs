20eccbc3642021224f5db055ee5ffe050c488c21
BillyHarrington1 <datskiydenis@gmail.com>
2025-09-27 23:58:10 +0500

    fix task7

--- Tyuiu.DatskiyDE.Sprint2.Task7.V9.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint2.Task7.V9.Lib/DataService.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint2;
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
 namespace Tyuiu.DatskiyDE.Sprint2.Task7.V9.Lib
 {
     public class DataService : ISprint2Task7V9
     {
         public bool CheckDotInShadedArea(double x, double y)
-        {
-            // выше ли синус y = sin(x)
-            // и ниже ли линии y = 0.5
-            bool aboveSin = y >= Math.Sin(x);
-            bool belowLine = y <= 0.5;
-
-            // ограничение области
+        {      
+            bool aboveSin = y > Math.Sin(x);
+            bool belowLine = y < 0.5;       
             bool inXRange = x >= -Math.PI && x <= Math.PI;
 
             return aboveSin && belowLine && inXRange;
